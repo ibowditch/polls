ff6b0786e5a90e054a483d6d1b715014c58834be
ibowditch <ibowditch@gmail.com>
2019-08-15 09:45:41 +1000

    Updated settings for aws. Added createsu.

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -181,4 +181,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "polls", "static")
 STATIC_URL = '/static/'
